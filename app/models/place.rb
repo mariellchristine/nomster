e6692d566f31c5135e463a07f330524b4b728b09
vagrant <vagrant@precise32.(none)>
2016-07-10 06:23:13 +0000

    add additional validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, length: {minimum: 3}, presence: true
+  validates :description, :address, presence: true
 end
 
