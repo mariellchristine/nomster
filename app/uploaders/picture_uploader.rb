ff52c5a0fc0afc0457655396194e79f041ba6ec0
vagrant <vagrant@precise32.(none)>
2016-08-16 03:07:35 +0000

    switch carrierwave to use s3

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,13 +3,13 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-    include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
-    process resize_to_fill: [200, 200]
+
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -33,9 +33,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-   version :thumb do
-     process :resize_to_fit => [50, 50]
-   end
+  # version :thumb do
+  #  process :resize_to_fit => [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
